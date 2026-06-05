90900afa908e6d820f673780f7489abd51cc6ff6
Facundo Ignacio López <FacundoIgnacio.Lopez@alumnos.uai.edu.ar>
2026-06-05 20:42:15 -0300

    a

---
+++ BE/ComponentePermiso.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public abstract class ComponentePermiso
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+
+        public abstract bool Tiene(string codigo);
+
+        public virtual void Agregar(ComponentePermiso componente)
+        {
+            throw new InvalidOperationException(
+                "No se pueden agregar permisos a una patente.");
+        }
+
+        public virtual void Quitar(ComponentePermiso componente)
+        {
+            throw new InvalidOperationException(
+                "No se pueden quitar permisos de una patente.");
+        }
+
+        public virtual IReadOnlyList<ComponentePermiso> ObtenerHijos()
+        {
+            throw new InvalidOperationException(
+                "Una patente no contiene hijos.");
+        }
+    }
+}
---
+++ BE/GrupoPermiso.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class GrupoPermiso : ComponentePermiso
+    {
+        private readonly List<ComponentePermiso> _hijos = new List<ComponentePermiso>();
+
+        public override bool Tiene(string codigo)
+        {
+            if (this.Codigo == codigo) return true;
+            foreach (var hijo in _hijos)
+            {
+                if (hijo.Tiene(codigo)) return true;
+            }
+            return false;
+        }
+
+        public override void Agregar(ComponentePermiso componente)
+        {
+            if (componente == null)
+                throw new System.ArgumentNullException(nameof(componente));
+            if (componente == this)
+                throw new System.InvalidOperationException(
+                    "Una familia no puede contenerse a sí misma.");
+            _hijos.Add(componente);
+        }
+
+        public override void Quitar(ComponentePermiso componente)
+        {
+            if (!_hijos.Remove(componente))
+                throw new System.InvalidOperationException(
+                    "El componente no pertenece a esta familia.");
+        }
+
+        public override IReadOnlyList<ComponentePermiso> ObtenerHijos()
+        {
+            return _hijos.AsReadOnly();
+        }
+    }
+}
